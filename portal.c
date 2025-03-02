c0bfc5d19d966367788683b7ba3271e3ebf30f6a
furis <yukannahinoishi@gmail.com>
2025-03-03 06:02:42 +0700

    sudah jadi portal tapi masih ada bug

--- portal.c
+++ portal.c
@@ -1,17 +1,107 @@
 #include "raylib.h"
+#include <stdio.h>
+#include <stdbool.h>
+#define LEBAR_LAYAR 800
+#define TINGGI_LAYAR 650
 
-   struct portal{
-       
-};
 
-struct peluru{
-const int cooldown;
-int coorX;
-int coorY;
-};
-int main() {
-    struct peluru P1={10,0,0};
+
+int lastbutton(){
+ if(IsKeyDown(KEY_RIGHT)){
+
+return 262;
+}
+else if(IsKeyDown(KEY_LEFT)){
+
+return 263;
+}
+else if(IsKeyDown(KEY_DOWN)){
+
+return 264;
+}
+else if(IsKeyDown(KEY_UP)){
+
+return 265;
+}
+
+}
+
+
+   void portal_blast(int coorX,int coorY,int *buffer){
+       float radius=30;
+    switch(*buffer){
+        case 262: //right
+        while(coorX < LEBAR_LAYAR){
+        coorX +=20;
+        BeginDrawing();
+        DrawCircle(coorX,coorY,radius,GOLD);
+        ClearBackground(WHITE);
+        EndDrawing();
+        }
+        ClearBackground(WHITE);
+        break;
+        
+        case 263: //left
+        while(coorX>LEBAR_LAYAR-800){
+        coorX -=20;
+        BeginDrawing();
+        DrawCircle(coorX,coorY,radius,GOLD);
+        ClearBackground(WHITE);
+        EndDrawing();
+            printf("huaaa%d\n",lastbutton());
+        }
+        ClearBackground(WHITE);
+        break;
+        
+        case 264: //down
+        while(coorY>TINGGI_LAYAR-650){
+        coorY -=20;
+        BeginDrawing();
+        DrawCircle(coorX,coorY,radius,GOLD);
+        ClearBackground(WHITE);
+        EndDrawing();
+        }
+        ClearBackground(WHITE);
+        break;
+        
+        case 265: //up
+        while(coorY<TINGGI_LAYAR){
+        coorY +=20;
+        BeginDrawing();
+        DrawCircle(coorX,coorY,radius,GOLD);
+        ClearBackground(WHITE);
+        EndDrawing();
+        }
+        ClearBackground(WHITE);
+        break;
+        }
+        ClearBackground(WHITE);
+        printf("a%d\n",lastbutton());
+    }
+   
+
+
+
+int main(){
+ 
+    int buffer;
     // Inisialisasi window
-    InitWindow(800, 600, "Gerakkan Lingkaran");
+    InitWindow(LEBAR_LAYAR,TINGGI_LAYAR, "Gerakkan Lingkaran");
     SetTargetFPS(60);
+     while(!WindowShouldClose()){
+         buffer=263;
+         printf("pppp%d\n",buffer);
+           if(IsKeyDown(KEY_P)){
+            portal_blast(200,300,&buffer);
+        }
+
+         BeginDrawing();
+    ClearBackground(GOLD);
+    DrawCircle(400,400,90,BLUE);
+    EndDrawing();
+      
+      
+     }
+         CloseWindow();
+    return 0;
 }
--- test.c
+++ test.c
@@ -1,13 +1,22 @@
 #include "raylib.h"
+#include <stdio.h>
 
-int main(){
-    InitWindow(900,650,"testo");
-    while(!WindowShouldClose()){
-    BeginDrawing();
-    ClearBackground(GOLD);
-    DrawCircle(810,200,90,BLUE);
-    EndDrawing();
+int main() {
+    InitWindow(800, 600, "Cek Tombol Pertama yang Ditekan");
+
+    while (!WindowShouldClose()) {
+        int key = GetKeyPressed(); // Mendapatkan tombol yang ditekan
+
+        if (key != 0) { // Jika ada tombol yang ditekan
+            printf("Tombol pertama yang ditekan: %d (%c)\n", key, (char)key);
+        }
+
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Tekan tombol apa saja...", 200, 250, 20, DARKGRAY);
+        EndDrawing();
     }
-    
-    
+
+    CloseWindow();
+    return 0;
 }
