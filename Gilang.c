9e0a3814ef77296c88a0f0381bc1e662ff3ebdc9
Gilang <adityasumarna21@gmail.com>
2025-03-02 17:05:03 +0700

    Remove main, just function/procedure

--- Gilang.c
+++ Gilang.c
@@ -5,31 +5,6 @@
 #define TINGGI_LAYAR 800
 #define UKURAN_BLOCK 20
 
-int main(){
-    
-    InitWindow(LEBAR_LAYAR, TINGGI_LAYAR, "Snake Game");
-    Snake Snake;
-    SetTargetFPS(10);
-    InitSnake(&Snake);
-
-    while(!WindowShouldClose()){
-        BeginDrawing();
-        ClearBackground(BLACK);
-        if(!cekTabrakan(&Snake)){
-            UpdateSnake(&Snake);
-            DrawSnake(&Snake);
-        }
-        else{
-            DrawText("Game Over", LEBAR_LAYAR/2, TINGGI_LAYAR/2, 25, RED);
-        }
-        EndDrawing();
-
-    }
-
-    CloseWindow();
-    return 0;
-}
-
 void InitSnake(Snake *Snake){
     Snake->position = (Vector2){LEBAR_LAYAR/2, TINGGI_LAYAR/2};
     Snake->speed= (Vector2){UKURAN_BLOCK, 0};
