780ebb08720d72950b6e09070472a1e155d86d2a
Gilang <adityasumarna21@gmail.com>
2025-03-02 18:06:53 +0700

    Forgot the file

---
+++ TestBranch.c
@@ -0,0 +1,34 @@
+#include "raylib.h"
+#include <stdlib.h>
+#include "Gilang.h"
+#include "Gilang.c"
+#define LEBAR_LAYAR 600
+#define TINGGI_LAYAR 800
+#define UKURAN_BLOCK 20
+
+int main(){
+    
+    InitWindow(LEBAR_LAYAR, TINGGI_LAYAR, "Snake Game");
+    Snake Snake;
+    SetTargetFPS(10);
+    InitSnake(&Snake);
+
+    while(!WindowShouldClose()){
+        BeginDrawing();
+        ClearBackground(BLACK);
+        
+
+        if(!cekTabrakan(&Snake)){
+            UpdateSnake(&Snake);
+            DrawSnake(&Snake);
+        }
+        else{
+            DrawText("Game Over", LEBAR_LAYAR/2, TINGGI_LAYAR/2, 25, RED);
+        }
+        EndDrawing();
+
+    }
+
+    CloseWindow();
+    return 0;
+}
