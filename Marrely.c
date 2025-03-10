b8d0101aecc570dd68456f95232ecd9284795c7c
marrelykasih <marrely.salsa.tif24@polban.ac.id>
2025-03-10 09:38:09 +0700

    meng update codingan sebelumnya

--- Marrely.c
+++ Marrely.c
@@ -1,8 +1,9 @@
+
 #include "Marrely.h"
 #include <stdlib.h>
 #include <time.h>
 
-void GenerateMakanan(Makanan *makanan, Rintangan *rintangan, Snake *snake) {
+void GenerateMakanan(Makanan *makanan, Rintangan *rintangan) {
     int validPosition = 0;
     while (!validPosition) {
         makanan->position.x = rand() % GRID_WIDTH;
@@ -21,11 +22,11 @@ void GenerateMakanan(Makanan *makanan, Rintangan *rintangan, Snake *snake) {
 void GenerateRintangan(Rintangan *rintangan, int level) {
     rintangan->count = level;
     Position levelRintangan[5][10] = {
-        { {5, 5}, {10, 10}, {15, 15} },
-        { {3, 4}, {7, 8}, {11, 12}, {15, 16} },
-        { {2, 2}, {6, 6}, {10, 10}, {14, 14}, {18, 18} },
-        { {1, 3}, {5, 7}, {9, 11}, {13, 15}, {17, 19}, {20, 22} },
-        { {0, 0}, {4, 4}, {8, 8}, {12, 12}, {16, 16}, {20, 20}, {24, 24} }
+        { {5, 5}, {10, 10}, {15, 15} },  
+        { {3, 4}, {7, 8}, {11, 12}, {15, 16} },  
+        { {2, 2}, {6, 6}, {10, 10}, {14, 14}, {18, 18} },  
+        { {1, 3}, {5, 7}, {9, 11}, {13, 15}, {17, 19}, {20, 22} },  
+        { {0, 0}, {4, 4}, {8, 8}, {12, 12}, {16, 16}, {20, 20}, {24, 24} }  
     };
 
     for (int i = 0; i < rintangan->count; i++) {
@@ -33,35 +34,7 @@ void GenerateRintangan(Rintangan *rintangan, int level) {
     }
 }
 
-void InitSnake(Snake *snake) {
-    snake->length = 1;
-    snake->body[0].x = GRID_WIDTH / 2;
-    snake->body[0].y = GRID_HEIGHT / 2;
-    snake->dx = 1;
-    snake->dy = 0;
-}
-
-void UpdateSnake(Snake *snake) {
-    for (int i = snake->length - 1; i > 0; i--) {
-        snake->body[i] = snake->body[i - 1];
-    }
-    snake->body[0].x += snake->dx;
-    snake->body[0].y += snake->dy;
-}
-
-int CheckCollision(Snake *snake, Rintangan *rintangan) {
-    for (int i = 0; i < rintangan->count; i++) {
-        if (snake->body[0].x == rintangan->rintangan[i].x && snake->body[0].y == rintangan->rintangan[i].y) {
-            return 1;
-        }
-    }
-    if (snake->body[0].x < 0 || snake->body[0].x >= GRID_WIDTH || snake->body[0].y < 0 || snake->body[0].y >= GRID_HEIGHT) {
-        return 1;
-    }
-    return 0;
-}
-
-void DrawGame(Makanan *makanan, Rintangan *rintangan, Snake *snake, int score, int lives, int level, Texture2D background) {
+void DrawGame(Makanan *makanan, Rintangan *rintangan, int score, int lives, int level, Texture2D background) {
     ClearBackground(RAYWHITE);
     DrawTexture(background, 0, 0, WHITE);
     
@@ -69,10 +42,6 @@ void DrawGame(Makanan *makanan, Rintangan *rintangan, Snake *snake, int score, i
 
     for (int i = 0; i < rintangan->count; i++) {
         DrawRectangle(rintangan->rintangan[i].x * CELL_SIZE, rintangan->rintangan[i].y * CELL_SIZE, CELL_SIZE, CELL_SIZE, BLACK);
-    }
-
-    for (int i = 0; i < snake->length; i++) {
-        DrawRectangle(snake->body[i].x * CELL_SIZE, snake->body[i].y * CELL_SIZE, CELL_SIZE, CELL_SIZE, GREEN);
     }
 
     DrawText(TextFormat("Score: %d", score), 10, 10, 20, BLACK);
--- Marrely.h
+++ Marrely.h
@@ -1,5 +1,5 @@
-#ifndef GAME_H
-#define GAME_H
+#ifndef MARRELY_H
+#define MARRELY_H
 
 #include "raylib.h"
 
@@ -8,7 +8,7 @@
 #define GRID_HEIGHT (400 / CELL_SIZE)  
 #define SCREEN_WIDTH  500
 #define SCREEN_HEIGHT 400
-#define MAX_LENGTH 100
+#define BORDER_THICKNESS 30
 
 typedef struct {
     int x, y;
@@ -23,17 +23,8 @@ typedef struct {
     int count;
 } Rintangan;
 
-typedef struct {
-    Position body[MAX_LENGTH];
-    int length;
-    int dx, dy;
-} Snake;
-
-void GenerateMakanan(Makanan *makanan, Rintangan *rintangan, Snake *snake);
+void GenerateMakanan(Makanan *makanan, Rintangan *rintangan);
 void GenerateRintangan(Rintangan *rintangan, int level);
-void InitSnake(Snake *snake);
-void UpdateSnake(Snake *snake);
-int CheckCollision(Snake *snake, Rintangan *rintangan);
-void DrawGame(Makanan *makanan, Rintangan *rintangan, Snake *snake, int score, int lives, int level, Texture2D background);
+void DrawGame(Makanan *makanan, Rintangan *rintangan, int score, int lives, int level, Texture2D background);
 
 #endif
