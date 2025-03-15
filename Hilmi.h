5e03b1c7c852ca318cd1bf16b858b283d3d34aae
hilmimahdani <hilmimahdanib@gmail.com>
2025-03-15 17:48:43 +0700

    hapus struct, include Gilang.h

--- Hilmi.h
+++ Hilmi.h
@@ -1,14 +1,8 @@
 #include <stdlib.h>
 #include "raylib.h"
+#include "Gilang.h"
 #ifndef Hilmi_H
 #define Hilmi_H
-
-typedef struct{
-  Vector2 tempBody [2000];
-  Vector2 position;
-  Vector2 speed;
-  int panjang;
-  } SNAKE;
 
 void ReverseSnake(SNAKE *SnakE);
 
