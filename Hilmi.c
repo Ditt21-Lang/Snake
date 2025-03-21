7e0ee0dc9e17419f4966b9c3dd99dcf1e5bfc12a
hilmimahdani <hilmimahdanib@gmail.com>
2025-03-21 15:11:02 +0700

    perbaikan reverse snake

--- Hilmi.c
+++ Hilmi.c
@@ -3,46 +3,18 @@
 #include "Hilmi.h"
 #include "Gilang.h"
 
-void ReverseSnake(Snake *Snake){
-    Vector2 tempBody[100];
-
-    for (int i = 0; i < Snake->Panjang; i++){
-        tempBody[i] = Snake->Body[Snake->Panjang - 1 - i];
-        }    /* menyimpan tubuh ular dalam urutan terbalik */
-
-    for (int i = 0; i < Snake->Panjang; i++){
-        Snake->Body[i] = tempBody[i];
-        }    /* mengganti posisi tubuh ular dengan tubuh yang sudah dibalik */
-
-    Snake->Position = Snake->Body[0]; /* mengatur posisi kepala sesuai dengan tubuh yang baru dibalik */
-
-    if(Snake->Speed.X !=0) {
-        Snake->Speed.X = -Snake->Speed.X; }
-    if(Snake->Speed.Y !=0) {
-        Snake->Speed.Y = -Snake->Speed.Y; }
+void ReverseSnake(Snake *snake) {
+    for (int i = 0; i < snake->length / 2; i++) {
+        Vector2 temp = snake->body[i];
+        snake->body[i] = snake->body[snake->length - 1 - i];
+        snake->body[snake->length - 1 - i] = temp;
+     }
+    snake->position = snake->body[0];
+    Vector2 diff = (Vector2){ snake->body[0].x - snake->body[1].x, 
+                              snake->body[0].y - snake->body[1].y };
+    snake->speed = diff;
+}
 
     if (IsKeyPressed(KEY_SPACE)) {
-        ReverseSnake(Snake);
-        }
-}
-
-int CheckBorderCollision(Position snakeHead) {
-    if (snakeHead.x < 0 || snakeHead.x >= SCREEN_WIDTH / CELL_SIZE ||
-        snakeHead.y < 0 || snakeHead.y >= SCREEN_HEIGHT / CELL_SIZE) {
-        return 1; // Kena border
+        ReverseSnake(&snake);
     }
-    return 0;
-}
-
-int CheckObstacleCollision(Position snakeHead, Rintangan *rintangan) {
-    for (int i = 0; i < rintangan->count; i++) {
-        if (snakeHead.x == rintangan->rintangan[i].x && 
-            snakeHead.y == rintangan->rintangan[i].y) {
-            return 1; // Kena rintangan
-        }
-    }
-    return 0;
-}
-
-
-
