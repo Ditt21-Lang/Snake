340681d2516f7dd0f34ada61a94ebdfea107e6f4
marrelykasih <marrely.salsa.tif24@polban.ac.id>
2025-03-22 09:49:39 +0700

    nyobain nya di npp jadi pas contol A yg gilang ga sengaja kehapus karna di npp pure bagian visual tadi

--- main.c
+++ main.c
@@ -1,43 +1,67 @@
 #include "raylib.h"
 #include <stdlib.h>
+#include "Gilang.h"
+#include "Marrely.h"
 #include <time.h>
-#include "marrely.h"
 
 #define LEBAR_LAYAR 600
 #define TINGGI_LAYAR 800
+#define UKURAN_BLOCK 20
 
 int main() {
     InitWindow(LEBAR_LAYAR, TINGGI_LAYAR, "Snake Game");
     
-    Snake snake;
+    Snake Snake;
     Rintangan obstacle;
     Makanan food;
-    int score = 0, level = 1; 
+    Enemy musuh;
 
-    InitSnake(&snake);
     SetTargetFPS(10);
-    srand(time(NULL));
+    InitSnake(&Snake);
+
 
     Texture2D background = LoadTexture("tanah.png");  
     Texture2D makananTexture = LoadTexture("tikus.png");  
     Texture2D rintanganTexture = LoadTexture("obstacle.png");  
     Texture2D enemyTexture = LoadTexture("krtTambang.png");
 
+    srand(time(NULL));
+    
+    int score = 0, lives = 3, level = 4;
+    
     GenerateRintangan(&obstacle, level);
     GenerateMakanan(&food, &obstacle);
-
-    int enemyCount = (level == 2) ? 1 : (level == 3) ? 2 : (level == 5) ? 3 : 0;
-    Enemy enemies[3];
-
-    if (enemyCount > 0) {
-        GenerateEnemy(enemies, enemyCount, level);
-    }
-
+    
     while (!WindowShouldClose()) {
         BeginDrawing();
-        DrawGame(&food, &obstacle, enemies, enemyCount, score, level, background, makananTexture, enemyTexture, rintanganTexture);
+        
+       DrawGame(&food, &obstacle, &musuh, 0, score, level, background, makananTexture, enemyTexture, rintanganTexture);
+
+        if (!cekTabrakan(&Snake)) {
+            UpdateSnake(&Snake);
+            DrawSnake(&Snake);
+        } else {
+            DrawText("Game Over", LEBAR_LAYAR / 2, TINGGI_LAYAR / 2, 25, RED);
+        }
+
+        if (CheckMakanan(&Snake, &food)) {
+            score += 100;
+            Snake.panjang++;
+            GenerateMakanan(&food, &obstacle);
+        }
+
+        if (CheckObstacle(&Snake, &obstacle)) {
+            score -= 50;
+            Snake.panjang--;
+        }
+        
         EndDrawing();
     }
+
+    UnloadTexture(background);
+    UnloadTexture(makananTexture);
+    UnloadTexture(rintanganTexture);
+    UnloadTexture(enemyTexture);
 
     CloseWindow();
     return 0;
