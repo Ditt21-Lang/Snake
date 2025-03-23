f8b5b607daee3eed8de0d3c762a7cfaa819f35ab
hilmimahdani <hilmimahdanib@gmail.com>
2025-03-23 23:36:39 +0700

    update

--- Hilmi.h
+++ Hilmi.h
@@ -6,28 +6,14 @@
 #include "Gilang.h"
 #include "Marrely.h"
 #include <stdbool.h>
-#define GRID_WIDTH 30
-#define GRID_HEIGHT 30
 #define REVERSE_COOLDOWN 20.0f
-
-typedef struct{
-    Position rintangan[10];
-    int count;
-} Rintangan;
-
-typedef struct{
-    Position position;
-    int direction;
-    int isVertical;
-    int count;
-} Enemy;
 
 extern float reverseTimer;
 
 void ReverseSnake(Snake *Snake);
 void HandleReverseInput(Snake *Snake);
 void UpdateCooldown();
-void CekTabrakDinding(Vector2 head, int *lives, bool *alive);
+void CekTabrakDinding(Snake *snake, int *lives, bool *alive);
 void CekTabrakRintangan(Vector2 head, Rintangan *rintangan, int *lives);
 void CekTabrakEnemy(Vector2 head, Enemy *enemy, int *count, int *lives);
 
