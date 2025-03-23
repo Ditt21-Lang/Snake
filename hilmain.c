aca238d902e8b9e85a4d3ece8aa3c59e56fe70e3
hilmimahdani <hilmimahdanib@gmail.com>
2025-03-23 20:18:13 +0700

    nambah pemanggilan reverse

--- hilmain.c
+++ hilmain.c
@@ -1,20 +1,39 @@
 #include <stdio.h>
-#include "Marrely.h"
+#include "raylib.h"
+#include "stdlib.h"
 #include "Hilmi.h"
+#include "Gilang.h"
+#include "Marrely.h"
+#include "dinding.h"
+#define GRID_WIDTH 30
+#define GRID_HEIGHT 30
 
-int main() {
+int main (){
+    Vector2 snakeHead = {12, 12};
+    int lives = 3;
+    int count = 0;
+    bool alive = true;
+    int level = 1;
+
+    Snake snake;
     Rintangan rintangan;
     Enemy enemy[3];
-    Vector2 snakeHead = {12, 12}; // Kepala ular
-    int level = 2;
-    int lives = 3;
 
-    
     GenerateRintangan(&rintangan, level);
-    GenerateEnemy(enemy, level, level);
 
-    CekTabrakRintangan(snakeHead, &rintangan, NULL, &lives);
-    CekTabrakEnemy(snakeHead, enemy, level, &lives);
+    if (level >= 2){
+    GenerateEnemy(enemy, 3, level);
+    }
 
-    return 0;
+    while (!WindowShouldClose() && alive) {
+        CekTabrakDinding(snakeHead, &lives, &alive);
+
+        CekTabrakRintangan(snakeHead, &rintangan, lives);
+
+        if (level >= 2){
+            CekTabrakEnemy(snakeHead, enemy, count, lives);
+        }
+        UpdateCooldown();
+        HandleReverseInput(&snake);
+    }
 }
