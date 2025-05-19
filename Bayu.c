c27d0e32ed34d9ef276450306ea853f88b86fee0
byebay <bayuuua00@gmail.com>
2025-05-19 08:25:22 +0700

    menghapus fungsi yang tidak terpakai

--- Bayu.c
+++ Bayu.c
@@ -19,26 +19,6 @@ void AppendButton(ButtonNode** head, const char* text, int yPos) {
     }
 }
 
-ButtonNode* CreateButton(const char* text, int yPos) {
-    ButtonNode* node = (ButtonNode*)malloc(sizeof(ButtonNode));
-    node->border = (Rectangle){200, yPos, 200, 50};
-    node->text = (char*)malloc(strlen(text) + 1);
-    strcpy(node->text, text);
-    node->hover = false;
-    node->next = NULL;
-    return node;
-}
-
-ButtonNode* TextNode(const char* text, int yPos) {
-    ButtonNode* node = (ButtonNode*)malloc(sizeof(ButtonNode));
-    node->border = (Rectangle){200, yPos, 200, 50};
-    node->text = (char*)malloc(strlen(text) + 1);
-    strcpy(node->text, text);
-    node->hover = false;
-    node->next = NULL;
-    return node;
-}
-
 void UpdateButtons(ButtonNode *head, GameScreen *screen) {
     Vector2 mousePoint = GetMousePosition();
     int i = 0;
